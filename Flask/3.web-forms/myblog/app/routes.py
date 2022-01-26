b5ca72b0c2f789f28f391542269241bb2965388b
Your Name <you@example.com>
2022-01-26 22:49:50 +0700

    update web-forms
    Signed-off-by: Your Name <you@example.com>

--- Flask/3.web-forms/myblog/app/routes.py
+++ Flask/3.web-forms/myblog/app/routes.py
@@ -1,8 +1,29 @@
-from flask import render_template
+from crypt import methods
+from curses import flash
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
-@app.route('/login')
+
+@app.route("/")
+@app.route("/index")
+def index():
+    user = {"username": "Khanh"}
+    posts = [
+        {"author": {"username": "Nguyen"}, "body": "Flask de hoc qua phai khong"},
+        {"author": {"username": "Tran"}, "body": "Lap trinh Web that thu vi"},
+    ]
+    return render_template("index.html", title="Home", user=user, posts=posts)
+
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
-    return render_template('login.html', title='Sign In', form=form)
+    if form.validate_on_submit():
+        flash(
+            "Yeu cau dang nhap tu User {}, remember_me={}".format(
+                form.username.data, form.remember_me.data
+            )
+        )
+        return redirect('/index')
+    return render_template("login.html", title="Sign In", form=form)
